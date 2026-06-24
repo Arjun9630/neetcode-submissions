7308bb13b0422b9773acae286a3a1e551bc107ce
Arjun.P <134117786+Arjun9630@users.noreply.github.com>
2026-06-24 09:35:02 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        encoded_str = ""
+        for strr in strs:
+            for char in strr:
+                asci = ord(char)
+                encoded_asci = (asci+6)%255
+                encoded_str += chr(encoded_asci)
+            encoded_str += " "
+        return encoded_str
+
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return []
+        decoded_str = ""
+        decoded_str_list = []
+        for chrr in s:
+            if chrr == " ":
+                decoded_str_list.append(decoded_str)
+                decoded_str = ""
+                continue
+            asci = ord(chrr)
+            decoded_asci = (asci-6)%255
+            decoded_str += chr(decoded_asci)
+        return decoded_str_list
